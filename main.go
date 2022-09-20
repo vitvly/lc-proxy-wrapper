dcffb5d2b8b83f59fe1b96c1b1500b69261299fa
Vitaliy Vlasov <siphiuel@protonmail.com>
2022-09-20 19:17:45 +0300

    Add callbacks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"os"
+	"unsafe"
 
 	"os/signal"
 	"syscall"
@@ -30,19 +31,21 @@ func goCallback(json *C.char) {
 
 func main() {
 	fmt.Println("vim-go")
-	//cb := (C.callback_type)(unsafe.Pointer(C.goCallback_cgo))
+	cb := (C.callback_type)(unsafe.Pointer(C.goCallback_cgo))
 	C.testEcho()
-	// C.setOptimisticHeaderCallback(cb)
-	// C.setFinalizedHeaderCallback(cb)
+	C.setOptimisticHeaderCallback(cb)
+	C.setFinalizedHeaderCallback(cb)
 	fmt.Println("vim-go 2")
 	type Config struct {
-		Network          string
-		TrustedBlockRoot string
+		Eth2Network      string `toml:"network"`
+		TrustedBlockRoot string `toml:"trusted-block-root"`
 	}
 
 	var testConfig = Config{
-		Network:          "prater",
-		TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
+		Eth2Network:      "mainnet",
+		TrustedBlockRoot: "0x60d6462ea001c078ec95c4cccb7982e82503b99bd5fd91a5b5ed06a0d736fa6f",
+		//Eth2Network:      "prater",
+		//TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
 	}
 
 	var buffer bytes.Buffer
@@ -50,14 +53,19 @@ func main() {
 	if err != nil {
 		return
 	}
-	var configStr = buffer.String()
-	configCStr := C.CString(configStr)
-	fmt.Println("configStr: ", configStr)
+	tomlFileName := "config.toml"
+	f, err := os.Create(tomlFileName)
+	if err != nil {
+		return
+	}
+	//defer f.Close()
+	f.WriteString(buffer.String())
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
+		configCStr := C.CString(tomlFileName)
 		C.startLc(configCStr)
 	}()
 	fmt.Println("vim-go 3")
