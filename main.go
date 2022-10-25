39d98242422ce7f70ea6f873ababb134dd5cf017
Vitaliy Vlasov <siphiuel@protonmail.com>
2022-10-25 15:40:52 +0300

    Extend config

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"os"
 	"unsafe"
@@ -30,6 +31,8 @@ type Config struct {
 	Eth2Network      string      `toml:"network"`
 	TrustedBlockRoot string      `toml:"trusted-block-root"`
 	Web3Url          Web3UrlType `toml:"web3-url"`
+	RpcAddress       string      `toml:"rpc-address"`
+	RpcPort          uint16      `toml:"rpc-port"`
 }
 
 type BeaconBlockHeader struct {
@@ -52,23 +55,15 @@ func goCallback(json *C.char) {
 	// fmt.Println("Unmarshal result: " + hdr)
 }
 
-func main() {
+func StartLightClient(ctx context.Context, cfg *Config) {
 	fmt.Println("vim-go")
 	cb := (C.callback_type)(unsafe.Pointer(C.goCallback_cgo))
 	C.testEcho()
 	C.setOptimisticHeaderCallback(cb)
 	C.setFinalizedHeaderCallback(cb)
 	fmt.Println("vim-go 2")
-	var testConfig = Config{
-		Eth2Network:      "mainnet",
-		TrustedBlockRoot: "0x6fd2f9fdc616a0755f3f88ac58e4a7871788c3128261a18bf9645eee7042eb53",
-		Web3Url:          Web3UrlType{"HttpUrl", "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938"},
-		//Eth2Network:      "prater",
-		//TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
-	}
-
 	var buffer bytes.Buffer
-	err := toml.NewEncoder(&buffer).Encode(testConfig)
+	err := toml.NewEncoder(&buffer).Encode(cfg)
 	if err != nil {
 		return
 	}
@@ -77,20 +72,47 @@ func main() {
 	if err != nil {
 		return
 	}
-	//defer f.Close()
+	defer f.Close()
 	f.WriteString(buffer.String())
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		configCStr := C.CString(tomlFileName)
 		C.startLc(configCStr)
+		fmt.Println("inside go-func after startLc")
+	}()
+	go func() {
+		fmt.Println("Before range ctx.Done()")
+		for range ctx.Done() {
+			fmt.Println("inside go-func ctx.Done()")
+			C.quit()
+		}
 	}()
 	fmt.Println("vim-go 3")
 
-	<-signals
-	//C.invokeHeaderCallback()
-	//C.testEcho()
+}
+
+func main() {
+	var testConfig = Config{
+		Eth2Network:      "mainnet",
+		TrustedBlockRoot: "0x6fd2f9fdc616a0755f3f88ac58e4a7871788c3128261a18bf9645eee7042eb53",
+		Web3Url:          Web3UrlType{"HttpUrl", "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938"},
+		RpcAddress:       "127.0.0.1",
+		RpcPort:          8545,
+		//Eth2Network:      "prater",
+		//TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	StartLightClient(ctx, &testConfig)
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Before range signals")
+	// time.Sleep(8 * time.Second)
+	// cancel()
+	for range signals {
+		fmt.Println("Signal caught, exiting")
+		cancel()
+	}
+	fmt.Println("Exiting")
 
 }
