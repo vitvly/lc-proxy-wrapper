aaa8e085c71eecce39b333b8a93cdf9d915688f1
Vitaliy Vlasov <siphiuel@protonmail.com>
2022-10-29 15:35:39 +0300

    Comment main()

--- main.go
+++ main.go
@@ -9,9 +9,6 @@ import (
 	"runtime"
 	"unsafe"
 
-	"os/signal"
-	"syscall"
-
 	"github.com/BurntSushi/toml"
 )
 
@@ -107,30 +104,30 @@ func StartLightClient(ctx context.Context, cfg *Config) {
 
 }
 
-func main() {
-	var testConfig = Config{
-		Eth2Network:      "mainnet",
-		TrustedBlockRoot: "0x6fd2f9fdc616a0755f3f88ac58e4a7871788c3128261a18bf9645eee7042eb53",
-		//Web3Url:          Web3UrlType{"HttpUrl", "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938"},
-		Web3Url:    "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938",
-		RpcAddress: "127.0.0.1",
-		RpcPort:    8545,
-		LogLevel:   "INFO",
-		//Eth2Network:      "prater",
-		//TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
-	}
-	ctx, cancel := context.WithCancel(context.Background())
-	StartLightClient(ctx, &testConfig)
+//func main() {
+//	var testConfig = Config{
+//		Eth2Network:      "mainnet",
+//		TrustedBlockRoot: "0x6fd2f9fdc616a0755f3f88ac58e4a7871788c3128261a18bf9645eee7042eb53",
+//		//Web3Url:          Web3UrlType{"HttpUrl", "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938"},
+//		Web3Url:    "https://mainnet.infura.io/v3/800c641949d64d768a5070a1b0511938",
+//		RpcAddress: "127.0.0.1",
+//		RpcPort:    8545,
+//		LogLevel:   "INFO",
+//		//Eth2Network:      "prater",
+//		//TrustedBlockRoot: "0x017e4563ebf7fed67cff819c63d8da397b4ed0452a3bbd7cae13476abc5020e4",
+//	}
+//	ctx, cancel := context.WithCancel(context.Background())
+//	StartLightClient(ctx, &testConfig)
 
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
-	fmt.Println("Before range signals")
-	// time.Sleep(8 * time.Second)
-	// cancel()
-	for range signals {
-		fmt.Println("Signal caught, exiting")
-		cancel()
-	}
-	fmt.Println("Exiting")
+//	signals := make(chan os.Signal, 1)
+//	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+//	fmt.Println("Before range signals")
+//	// time.Sleep(8 * time.Second)
+//	// cancel()
+//	for range signals {
+//		fmt.Println("Signal caught, exiting")
+//		cancel()
+//	}
+//	fmt.Println("Exiting")
 
-}
+//}
